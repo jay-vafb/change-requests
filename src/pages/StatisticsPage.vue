30ab8db9426ad9bac2d1038426191d43194d4b9c
Jay Call <jcall@vafb.com>
2022-09-20 15:23:14 -0400

    fleshing out statistics page

--- src/pages/StatisticsPage.vue
+++ src/pages/StatisticsPage.vue
@@ -1,7 +1,106 @@
 <template>
-  <q-page padding>
-    <!-- content -->
-    Hello
+  <q-page class="q-pa-lg q-gutter-y-md">
+    <div class="row justify-center q-gutter-x-md" style="width: 100%">
+      <div class="col-6 col-md-3">
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Approved Change Requests</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Manager approved: 26
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section>Board approved: 20</q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-6 col-md-3">
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Denied Change Requests</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Manager denied: 31
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section> Board denied: 7 </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row justify-center q-gutter-x-md" style="width: 100%">
+      <div class="col-6 col-md-3">
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Monthly Change Requests</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Manager approved: 26
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section>Board approved: 20</q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-6 col-md-3">
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Yearly Change Requests</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Manager denied: 31
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section> Board denied: 7 </q-card-section>
+        </q-card>
+      </div>
+    </div>
+
+    <div class="row justify-center q-gutter-x-md" style="width: 100%">
+      <div class="col-6 col-md-3">
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Total Change Requests</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Manager approved: 26
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section>Board approved: 20</q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-6 col-md-3">
+        <q-card flat bordered class="my-card">
+          <q-card-section>
+            <div class="text-h6">Average Change Requests</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            Manager denied: 31
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section> Board denied: 7 </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
