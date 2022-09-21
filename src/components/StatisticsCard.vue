7f6cc7d3b49f8b2c3f4e8a552ced7ae8ea42eb3b
Jay Call <jcall@vafb.com>
2022-09-21 15:32:52 -0400

    statistics front-end complete

---
+++ src/components/StatisticsCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-card flat bordered style="min-width: 18rem">
+    <q-card-section horizontal>
+      <q-card-section style="width: 100%">
+        <div class="text-caption text-grey">{{ title.toUpperCase() }}</div>
+        <div class="text-h4 q-pt-xs">{{ value.toLocaleString() }}</div>
+      </q-card-section>
+
+      <q-card-section class="flex justify-right items-center">
+        <q-avatar :color="color" text-color="primary" :icon="icon" />
+      </q-card-section>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "StatisticsCard",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "secondary",
+    },
+  },
+};
+</script>
--- src/pages/StatisticsPage.vue
+++ src/pages/StatisticsPage.vue
@@ -1,9 +1,67 @@
 <template>
-  <q-page class="flex flex-center"> Hi </q-page>
+  <q-page class="q-pa-lg q-gutter-sm">
+    <div class="row justify-center q-gutter-x-md">
+      <div class="col-12 col-md-3 q-mb-sm">
+        <StatisticsCard
+          title="Monthly Change Requests"
+          :value="135"
+          icon="calendar_month"
+          color="positive"
+        />
+      </div>
+
+      <div class="col-12 col-md-3">
+        <StatisticsCard
+          title="Yearly Change Requests"
+          :value="235"
+          icon="calendar_month"
+          color="info"
+        />
+      </div>
+    </div>
+
+    <div class="row justify-center q-gutter-x-md">
+      <div class="col-12 col-md-3 q-mb-sm">
+        <StatisticsCard
+          title="Total Approved"
+          :value="932"
+          icon="done"
+          color="accent"
+        />
+      </div>
+
+      <div class="col-12 col-md-3">
+        <StatisticsCard title="Total Denied" :value="1235" icon="close" />
+      </div>
+    </div>
+
+    <div class="row justify-center q-gutter-x-md">
+      <div class="col-12 col-md-3 q-mb-sm">
+        <StatisticsCard
+          title="Average Approval Time"
+          :value="15"
+          icon="timer"
+          color="negative"
+        />
+      </div>
+
+      <div class="col-12 col-md-3">
+        <StatisticsCard
+          title="Total Change Requests"
+          :value="2532"
+          icon="add"
+          color="warning"
+        />
+      </div>
+    </div>
+  </q-page>
 </template>
 
+<style lang="sass" scoped></style>
+
 <script>
+import StatisticsCard from "src/components/StatisticsCard.vue";
 export default {
-  // name: 'PageName',
+  components: { StatisticsCard },
 };
 </script>
