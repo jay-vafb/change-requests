28b64d2592ea1e902b4850417d4872ff0c9ba802
Jay Call <jcall@vafb.com>
2022-10-05 13:47:53 -0400

    moved icon to left side of stats card

--- src/components/StatisticsCard.vue
+++ src/components/StatisticsCard.vue
@@ -1,13 +1,13 @@
 <template>
   <q-card flat bordered>
     <q-card-section horizontal>
+      <q-card-section class="flex flex-center items-center">
+        <q-avatar :color="color" text-color="grey-1" :icon="icon" />
+      </q-card-section>
+
       <q-card-section style="width: 100%">
         <div class="text-caption text-grey">{{ title.toUpperCase() }}</div>
         <div class="text-h4 q-pt-xs" v-text="value"></div>
-      </q-card-section>
-
-      <q-card-section class="flex justify-right items-center">
-        <q-avatar :color="color" text-color="grey-1" :icon="icon" />
       </q-card-section>
     </q-card-section>
   </q-card>
