7b544a1c80875ad2c1ee1edc238860392dff8005
Jay Call <jcall@vafb.com>
2022-11-04 11:38:06 -0400

    function is string

--- src/pages/auth/SignInPage.vue
+++ src/pages/auth/SignInPage.vue
@@ -130,7 +130,7 @@ export default {
 
           TypingDNAVerify2FA({
             ...res.data,
-            callbackFn: typeDNACallback,
+            callbackFn: "typeDNACallback",
           });
         })
         .catch((error) => {
